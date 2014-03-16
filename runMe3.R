1f5de260b172b783ec004e88f04e2351f7b5a065
Ben Sheng <bsheng99@gmail.com>
2014-03-15 17:18:56 -0700

    Q4 progress
    made progress on Q4 and made edits to Q3

---
+++ runMe3.R
@@ -0,0 +1,49 @@
+library(XML)
+library(ggplot2)
+library(gridExtra)
+
+##Ting code
+source("q1.R")
+location <- c(37.786289,-122.405234)
+dist_km <- 10
+stationdata <- Stations(location, dist_km)
+
+
+##Ben code
+source("getCond2.R")
+startDt <- "2014-03-04"
+endDt <- "2014-03-04"
+weatherCond <- getCond(startDt, endDt, stationdata)
+
+
+#get weather conditions for a given time
+#just take first obs for now
+subsetCond <- data.frame()
+for (i in 1:nrow(stationdata)) {
+  subsetCond <- rbind(subsetCond, 
+                      weatherCond[[i]][1, c("tempC", "humPer", "windKm", 
+                                            "wdirDeg", "pressureMb", "precip_rateMm")])
+}
+#handle invalid weather conditions manually for now
+subsetCond[5:6,] <- NA
+subCondPws <- cbind(stationdata[, c("Id", "Lat", "Lon")], subsetCond)
+
+
+#plot
+ggTemp <- ggplot(subCondPws, aes(Lon, Lat))
+ggTemp <- ggTemp + geom_point(aes(colour = tempC))
+ggHum <- ggplot(subCondPws, aes(Lon, Lat))
+ggHum <- ggHum + geom_point(aes(colour = humPer))
+ggWSpeed <- ggplot(subCondPws, aes(Lon, Lat))
+ggWSpeed <- ggWSpeed + geom_point(aes(colour = windKm))
+ggWDir <- ggplot(subCondPws, aes(Lon, Lat))
+ggWDir <- ggWDir + geom_point(aes(colour = wdirDeg))
+ggPres <- ggplot(subCondPws, aes(Lon, Lat))
+ggPres <- ggPres + geom_point(aes(colour = pressureMb))
+ggPrec <- ggplot(subCondPws, aes(Lon, Lat))
+ggPrec <- ggPrec + geom_point(aes(colour = precip_rateMm))
+
+#output
+pdf("microclimate1.pdf")
+grid.arrange(ggTemp, ggHum, ggWSpeed, ggWDir, ggPres, ggPrec, nrow=3, ncol=2)
+dev.off()
